aa6c815dc2b4e3322baab248b15d2cf657718dba
anshulruhil02 <92406374+anshulruhil02@users.noreply.github.com>
2023-01-14 16:23:05 -0500

    Add files via upload

---
+++ TicTacToeAI/TicTacToeAI/ExtensionContentaView.swift
@@ -0,0 +1,11 @@
+//
+//  ExtensionContentaView.swift
+//  TicTacToeAI
+//
+//  Created by Anshul Ruhil on 2023-01-13.
+//
+
+import Foundation
+extension ContentView {
+    
+}
---
+++ TicTacToeAI/TicTacToeAI/TicTacToeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAIApp.swift
+//  TicTacToeAI
+//
+//  Created by Anshul Ruhil on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
